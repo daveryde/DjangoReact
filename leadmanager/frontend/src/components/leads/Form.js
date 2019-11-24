6e0bebf80e2b12065b5629023548491469d283a9
daveryde <d.mcordova87@gmail.com>
2019-11-24 12:22:21 -0700

    redux, actions, forms

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
 
 export class Form extends Component {
+  state = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log('submit');
+  };
+
   render() {
+    const { name, email, message } = this.state;
     return (
-      <div>
+      <div className='card card-body mt-4 mb-4'>
         <h1>Add Lead Form</h1>
+        <form onSubmit={this.onSubmit}>
+          <div className='form-group'>
+            <label>Name</label>
+            <input
+              type='text'
+              className='form-control'
+              name='name'
+              onChange={this.onChange}
+              value={name}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Email</label>
+            <input
+              type='text'
+              className='form-control'
+              name='email'
+              onChange={this.onChange}
+              value={email}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Message</label>
+            <input
+              type='text'
+              className='form-control'
+              name='message'
+              onChange={this.onChange}
+              value={message}
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-primary'>
+              submit
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
