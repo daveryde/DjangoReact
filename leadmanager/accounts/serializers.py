05c04edd3bf69e6dc8e9cb01e31827aa8f284caa
daveryde <d.mcordova87@gmail.com>
2019-11-24 14:06:11 -0700

    error handling and authentication

---
+++ leadmanager/accounts/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from django.contrib.models import User
+from django.contrib.auth import authenticate
+
+# User Serializer
+
+# Register Serializer
+
+# Login Serializer
